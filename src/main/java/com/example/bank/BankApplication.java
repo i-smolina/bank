b379583378533ebda483d62c7639ad5608d1cd43
irina <smolirinka@gmail.com>
2023-10-12 10:25:48 +0300

    add userContext

--- src/main/java/com/example/bank/BankApplication.java
+++ src/main/java/com/example/bank/BankApplication.java
@@ -1,8 +1,18 @@
 package com.example.bank;
 
+import com.example.bank.utils.UserContextInterceptor;
+import feign.RequestInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +22,13 @@ public class BankApplication {
 		SpringApplication.run(BankApplication.class, args);
 	}
 
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		RestTemplate template = new RestTemplate();
+		List<ClientHttpRequestInterceptor> interceptors = template.getInterceptors();
+        interceptors.add(new UserContextInterceptor());
+        template.setInterceptors(interceptors);
+        return template;
+	}
 }
